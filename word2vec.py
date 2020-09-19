b6711fb73177e49e32e102598cd79be20fb75803
satojkovic <satojkovic@gmail.com>
2020-09-19 20:31:38 +0900

    Update word2vec.py

--- word2vec.py
+++ word2vec.py
@@ -5,3 +5,14 @@ def load_word2vec_from_file(path):
             values = line.split()
             word2vec[values[0]] = list(map(float, values[1:]))
     return word2vec
+
+def load_wordnet(path):
+    syn2word, word2syn = {}, {}
+    with open(path, 'r') as f:
+        for line in f:
+            tokens = line.rstrip().split()
+            synset = tokens[0]
+            word = tokens[1].lower()
+            syn2word[synset] = word
+            word2syn[word] = synset
+    return syn2word, word2syn
