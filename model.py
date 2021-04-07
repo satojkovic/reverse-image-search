2c0b13df67fd66c37be4f141c2d3fbe27823c53f
satojkovic <satojkovic@gmail.com>
2021-04-07 21:06:50 +0900

    Update model.py

--- model.py
+++ model.py
@@ -1,12 +1,14 @@
 import torch
 from torch import nn
 import torch.nn.functional as F
+import torchvision.models as models
 
 
 class TrainerBase(nn.Module):
     def training_step(self, batch):
         images, targets = batch
         out = self(images)
+        # Not same as the orignal paper
         loss = 1 - F.cosine_similarity(out, targets).mean()
         return loss
 
@@ -25,6 +27,27 @@ class TrainerBase(nn.Module):
         print('Epoch [{}], train_loss: {:.4f}, val_loss: {:.4f}'.format(
             epoch, result['train_loss'], result['val_loss']
         ))
+
+
+class DeviseModel(TrainerBase):
+    def __init__(self, embed_dim=300):
+        super().__init__()
+        self.network = models.resnet18(pretrained=True)
+        # Replace last layer
+        num_features = self.network.fc.in_features
+        self.network.fc = nn.Sequential(
+            nn.Flatten(),
+            nn.Dropout(p=0.25),
+            nn.Linear(in_features=num_features,
+                      out_features=num_features // 2),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm1d(num_features // 2),
+            nn.Dropout(p=0.5),
+            nn.Linear(num_features//2, embed_dim)
+        )
+
+    def forward(self, xb):
+        return self.network(xb)
 
 
 if __name__ == "__main__":
