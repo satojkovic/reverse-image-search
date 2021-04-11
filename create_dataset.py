fc9267103368a2b9c5dd9f200ef103f9f9276499
satojkovic <satojkovic@gmail.com>
2021-04-11 21:26:49 +0900

    Update

--- create_dataset.py
+++ create_dataset.py
@@ -13,4 +13,13 @@ if __name__ == '__main__':
     ft_words = ft_vecs.get_words(include_freq=True)
     ft_word_dict = {k: v for k, v in zip(*ft_words)}
     ft_words = sorted(ft_word_dict.keys(), key=lambda x: ft_word_dict[x])
+    lc_vec_d = {w.lower(): ft_vecs.get_word_vector(w) for w in ft_words}
     print('fastText words:', len(ft_words))
+
+    # Load mapping file
+    caltech256_to_fasttext = {}
+    with open('caltech256_categories_to_fasttext.txt', 'r') as f:
+        for line in f:
+            elems = line.strip().split(',')
+            caltech256_to_fasttext[elems[0]] = elems[1]
+    print('Num. of valid categories:', len(caltech256_to_fasttext))
