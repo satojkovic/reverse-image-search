3878162632aa5d1294d7fff300ea404b372b3676
satojkovic <satojkovic@gmail.com>
2021-04-11 21:22:50 +0900

    Add new file

---
+++ map_caltech256_categories_to_fasttext.py
@@ -0,0 +1,37 @@
+import fasttext as ft
+import argparse
+import os
+from pathlib import Path
+import re
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--fasttext_path', required=True,
+                        help='Path to fasttext dir')
+    parser.add_argument('--object_categories_path', required=True,
+                        help='Path to 256_ObjectCategories directory')
+    args = parser.parse_args()
+
+    FASTTEXT_PATH = Path(args.fasttext_path)
+    ft_vecs = ft.load_model(str(FASTTEXT_PATH / 'cc.en.300.bin'))
+    ft_words = ft_vecs.get_words(include_freq=True)
+    ft_word_dict = {k: v for k, v in zip(*ft_words)}
+    ft_words = sorted(ft_word_dict.keys(), key=lambda x: ft_word_dict[x])
+
+    with open('caltech256_categories_to_fasttext.txt', 'w') as f:
+        for d in sorted(os.listdir(args.object_categories_path)):
+            name = d.split('.')[1]
+            if name == 'DS_Store':
+                continue
+            if name in ft_words:
+                f.writelines(','.join([name, name]))
+                f.writelines('\n')
+            else:
+                names = name.split('-')
+                new_name = ''.join(names[:-1]) if re.match(r'\d+',
+                                                           names[-1]) else ''.join(names)
+                if new_name in ft_words:
+                    f.writelines(','.join([name, new_name]))
+                    f.writelines('\n')
+                else:
+                    print('Not exist in fasttext:', name)
