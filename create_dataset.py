d9629d08327caadab85018004c68ed37cf907442
satojkovic <satojkovic@gmail.com>
2021-04-13 21:50:16 +0900

    Save pickle files

--- create_dataset.py
+++ create_dataset.py
@@ -1,11 +1,22 @@
 import fasttext as ft
 import argparse
 from pathlib import Path
+import numpy as np
+import pickle
+
+
+def dump(pickle_path, data):
+    with open(pickle_path, 'wb') as f:
+        pickle.dump(data, f)
+    print('Dumped:', pickle_path)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--fasttext_path', required=True,
                         help='Path to fasttext dir')
+    parser.add_argument('--object_categories_path', required=True,
+                        help='Path to 256_ObjectCategories directory')
     args = parser.parse_args()
 
     FASTTEXT_PATH = Path(args.fasttext_path)
@@ -23,3 +34,41 @@ if __name__ == '__main__':
             elems = line.strip().split(',')
             caltech256_to_fasttext[elems[0]] = elems[1]
     print('Num. of valid categories:', len(caltech256_to_fasttext))
+
+    test_classes = ["bear", "piano", "laptop", "syringe",
+                    "tomato", "calculator", "rifle", "dog", "floppy", "octopus"]
+    CALTECH256_PATH = Path(args.object_categories_path)
+    classes = []
+    train_images = []
+    train_image_word_vecs = []
+    test_images = []
+    test_image_word_vecs = []
+    for dir_name in CALTECH256_PATH.iterdir():
+        class_name = dir_name.name.rpartition('.')[-1]
+        if not class_name in caltech256_to_fasttext:
+            continue
+        class_name = caltech256_to_fasttext[class_name]
+        class_images = list(dir_name.iterdir())
+        if class_name in test_classes:
+            test_images += class_images
+            test_image_word_vecs += [lc_vec_d[class_name]] * len(class_images)
+        else:
+            classes.append(class_name)
+            train_images += class_images
+            train_image_word_vecs += [lc_vec_d[class_name]] * len(class_images)
+    train_image_word_vecs = np.stack(train_image_word_vecs)
+    test_image_word_vecs = np.stack(test_image_word_vecs)
+    print('train_image_word_vecs.shape {}, test_image_word_vecs.shape {}'.format(
+        train_image_word_vecs.shape, test_image_word_vecs.shape))
+
+    # Train : Val = 0.75 : 0.25
+    all_ids = np.arange(len(train_images))
+    np.random.shuffle(all_ids)
+    num_train_ids = int(len(all_ids) * 0.75)
+    train_ids, val_ids = all_ids[:num_train_ids], all_ids[num_train_ids:]
+    dump('train_images.pkl', np.array(train_images)[train_ids])
+    dump('val_images.pkl', np.array(train_images)[val_ids])
+    dump('test_images.pkl', np.array(test_images))
+    dump('train_image_vecs.pkl', train_image_word_vecs[train_ids])
+    dump('val_image_vecs.pkl', train_image_word_vecs[val_ids])
+    dump('test_image_vecs.pkl', test_image_word_vecs)
